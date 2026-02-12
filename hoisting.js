bc3f49aa82937a143d8bd699105ab892ee00aa85
ibrahim <codewithbryhmo@gmail.com>
2026-02-12 04:56:23 +0100

    the local scope shadowing the global scope and overide

--- hoisting.js
+++ hoisting.js
@@ -1,5 +1,23 @@
-let a = 10;
+/* let b = 10;
 {
-  console.log(a);
-  // var a = 15;
+  console.log(b);
+  var b = 15;
+} */
+
+
+let a = 5;
+
+{
+    console.log(a);
+    // let a = 10;cl
 }
+
+
+let a = 5;
+
+function test() {
+    console.log(a);
+    let a = 10;
+}
+
+test();
